a71c9a352a271cb8ae74814cbd9b2ccdf3678b9e
Md Golam Jakaria <58362149+MdGolamJakaria@users.noreply.github.com>
2020-03-07 20:45:18 +0600

    push

--- javascript/main.js
+++ javascript/main.js
@@ -4,7 +4,7 @@ loginBtn.addEventListener("click",function(){
     const loginArea = document.getElementById("login-section");
     loginArea.style.display="none";
 
-})
+});
 
 
 //////click create account button
@@ -14,7 +14,7 @@ createAccountBtn.addEventListener("click",function(){
     loginArea.style.display="none";
     const signupArea = document.getElementById("sign-up-form");
     signupArea.style.display="block";
-})
+});
 
 ////click already have an account
 const alreadyAccount = document.getElementById("already-account-btn");
@@ -23,7 +23,7 @@ alreadyAccount.addEventListener("click",function(){
     signupArea.style.display="none";
     const loginArea = document.getElementById("login-section");
     loginArea.style.display="block";
-})
+});
 
 /////click login
 const logInBtn = document.getElementById("loginBtn");
@@ -31,7 +31,10 @@ logInBtn.addEventListener("click",function(){
     const loginArea = document.getElementById("login-area");
     loginArea.style.display="none";
 
+    const pageTopMarginRemove= document.getElementById("login-page");
+
     const mainArea1 = document.getElementById("main-area-1");
     mainArea1.style.display = "block";
-})
+    pageTopMarginRemove.style.marginTop = "0px";
+});
 
