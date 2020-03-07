88bdcd54d48d48e0fe0c99b6511b5c5aa9d873b3
Md Golam Jakaria <58362149+MdGolamJakaria@users.noreply.github.com>
2020-03-07 17:04:24 +0600

    create account done

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,15 @@
 const createAccountBtn = document.getElementById("create-account-btn");
 createAccountBtn.addEventListener("click",function(){
-    const loginArea = document.getElementById("login-form");
-    loginArea.style.display = "none";
+    const loginArea = document.getElementById("login-section");
+    loginArea.style.display="none";
     const signupArea = document.getElementById("sign-up-form");
     signupArea.style.display="block";
+})
+
+
+const loginBtn = document.getElementById("loginBtn");
+loginBtn.addEventListener("click",function(){
+    const loginArea = document.getElementById("login-section");
+    loginArea.style.display="none";
+
 })
